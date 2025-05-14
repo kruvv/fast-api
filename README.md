cf70513c9a9593f44001c0f293ccaac6d4898f9b
kruvv <kruvv@mail.ru>
2025-05-14 15:45:01 +0300

    create app

---
+++ README.md
@@ -0,0 +1,8 @@
+# App FAST API
+
+[setting]
+source venv/bin/activate
+pip install -r ./requirements.txt
+
+[run app]
+uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,44 @@
+from typing import Annotated, Optional
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('database cleared!!!')
+    await create_tables()
+    print('database ready by work!')
+    yield
+    print('switchOff app')
+
+app = FastAPI(lifespan=lifespan)
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class STask(STaskAdd):
+    id: int
+
+
+
+
+tasks = []
+
+
+@app.post("/tasks")
+async def add_task(
+    task: Annotated[STaskAdd, Depends()],
+):
+    tasks.append(task)
+    return {'ok': True}
+
+# @app.get('/tasks')
+# def get_tasks():
+#     task = Task(name="Write this video")
+#     return {'data': task}
