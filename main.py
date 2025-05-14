b2a091d57a60bf337313ccceec36a00e86d41b16
kruvv <kruvv@mail.ru>
2025-05-14 16:50:33 +0300

    Add router and schemas

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from typing import Annotated, Optional
-from fastapi import Depends, FastAPI
-from pydantic import BaseModel
+from fastapi import  FastAPI
 from contextlib import asynccontextmanager
 from database import create_tables, delete_tables
+from router import router as task_router
 
 
 @asynccontextmanager
@@ -16,29 +15,4 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-class STaskAdd(BaseModel):
-    name: str
-    description: Optional[str] = None
-
-
-class STask(STaskAdd):
-    id: int
-
-
-
-
-tasks = []
-
-
-@app.post("/tasks")
-async def add_task(
-    task: Annotated[STaskAdd, Depends()],
-):
-    tasks.append(task)
-    return {'ok': True}
-
-# @app.get('/tasks')
-# def get_tasks():
-#     task = Task(name="Write this video")
-#     return {'data': task}
+app.include_router(task_router)
--- repository.py
+++ repository.py
@@ -1,11 +1,11 @@
 from database import new_session, TaskOrm
-from main import STaskAdd
+from schemas import STaskAdd, STask
 from sqlalchemy import select
 
 class TaskRepository:
     @classmethod
     async def add_one(cls, data: STaskAdd) -> int:
-        async with new_sessision() as session:
+        async with new_session() as session:
             task_dict = data.model_dump()
 
             task = TaskOrm(**task_dict)
@@ -17,9 +17,10 @@ class TaskRepository:
 
 
     @classmethod
-    async def find_all(cls):
-        async with new_sessision() as session:
+    async def find_all(cls) -> list[STask]:
+        async with new_session() as session:
             query = select(TaskOrm)
             result = await session.execute(query)
             task_models = result.scalars().all()
-            return task_models
+            task_schemas = [STask.model_validate(task_model) for task_model in task_models]
+            return task_schemas
